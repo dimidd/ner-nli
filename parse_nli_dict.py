f7d35492483c6b2f1d3e5c9e6edbd54a34cc71d2
nelzas <nelzas@gmail.com>
2015-11-04 12:07:16 +0200

    script to parse the nli thesaurus

---
+++ parse_nli_dict.py
@@ -0,0 +1,27 @@
+import xml.etree.ElementTree as ET
+# from lxml import etree
+
+
+def clear_element(e):
+    e.clear()
+    while e.getprevious() is not None:
+        del e.getparent()[0]
+
+
+def parse_file(filename):
+    with open(filename, "rb") as f:
+        context = ET.iterparse(f,
+                               tag="{http://www.loc.gov/MARC21/slim}record")
+        for action, elem in context:
+            parse_record(elem)
+            clear_element(elem)
+
+
+def parse_record(elem):
+    print(elem.tag)
+    # print(etree.tostring(elem))
+    for x in elem:
+        print(x)
+
+
+parse_file("nnl10all_head.xml")
