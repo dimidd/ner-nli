ddceb38d816cc1b97f3b2a2f277122cb7f58dfa0
nelzas <nelzas@gmail.com>
2015-11-11 14:37:59 +0200

    function extract_words_from_alto_xml with metadata

--- parse_nli_alto_book.py
+++ parse_nli_alto_book.py
@@ -1,15 +1,23 @@
 from lxml import etree
 
 
-def parse_file(filename):
+def extract_words_from_alto_xml(filename):
+    """
+    extract words from an xml file in alto format
+    return list of words, each word is accompanied with metadata
+    to locate its source
+
+    """
     with open(filename, "rb") as f:
         tree = etree.parse(f)
-    print(tree.getroot())
+    words = []
     for word in tree.xpath("//String[@CONTENT]"):
-        print("ID:", word.get("ID"),
-              "CONTENT:", word.get("CONTENT"),
-              )
+        words.append({"ID:": word.get("ID"),
+                      "CONTENT:": word.get("CONTENT"),
+                      "parent:": word.getparent().get("ID"),
+                      })
+    return words
 
 
 if __name__ == "__main__":
-    parse_file("books_alto_fmt/1227225-140-0100.xml")  # page 100 from some book
+    print(extract_words_from_alto_xml("books_alto_fmt/1227225-140-0100.xml"))
