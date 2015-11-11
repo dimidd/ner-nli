1f25906b651461ce814ad707a0d8928bb2ac7454
nelzas <nelzas@gmail.com>
2015-11-11 18:51:34 +0200

    add look_for_entities, and lookup functions with dumb search

--- parse_nli_alto_book.py
+++ parse_nli_alto_book.py
@@ -22,5 +22,28 @@ def extract_words_from_alto_xml(filename):
     return words
 
 
+def lookup(word, entities):
+    """
+        return first record in entities that match word
+    """
+    for record in entities:
+        if word == record['name']:
+            return record
+    return None
+
+
+def look_for_entities(words, entities):
+    res = []
+    for word in words:
+        t = lookup(word["CONTENT"], entities)
+        if t:
+            res.append(t)
+    return res
+
 if __name__ == "__main__":
-    print(extract_words_from_alto_xml("books_alto_fmt/1227225-140-0100.xml"))
+    words = extract_words_from_alto_xml("books_alto_fmt/1227225-140-0100.xml")
+    entities = [
+        {'id': 1, 'name': 'חייבים', },
+        ]
+    res = look_for_entities(words, entities)
+    print(res)
