7fa94597b6c093ffa4e8907d346f990f5a831d3c
lotan1 <lotanbs@gmail.com>
2015-12-16 12:44:41 +0200

    view_template

---
+++ Nnview/entts/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Entt
+
+admin.site.register(Entt)
---
+++ Nnview/entts/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EnttsConfig(AppConfig):
+    name = 'entts'
