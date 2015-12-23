b092df89c09a13b4a3d5f6dd96b751dfe027d568
nelzas <nelzas@gmail.com>
2015-12-23 19:50:15 +0200

    use phrase search in mongo db lookup

--- db_api.py
+++ db_api.py
@@ -1,3 +1,6 @@
+#! /usr/bin/python3
+# coding=utf-8
+
 import pymongo
 
 CL = pymongo.MongoClient()
@@ -6,6 +9,12 @@ C = DB.ents
 
 
 def lookup(alias):
-    res = C.find_one({"$text": {"$search": alias}})
+    alias_for_phrase_search = '\"{}\"'.format(alias)
+    res = C.find_one({"$text": {"$search": alias_for_phrase_search}})
 
     return res
+
+
+if __name__ == "__main__":
+    print(C.count())
+    print(lookup("Leon Pol"))
