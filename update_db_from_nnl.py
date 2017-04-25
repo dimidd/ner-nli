bb35629fb336eead4170bd45219a3128c51234e4
Nethanel Elzas <nelzas@gmail.com>
2017-04-25 23:19:48 +0300

    new function xml_record_2_authority, and add result to mongo

--- update_db_from_nnl.py
+++ update_db_from_nnl.py
@@ -3,12 +3,14 @@
 from sickle import Sickle
 from pprint import pprint
 from lxml import etree, objectify
+import xmltodict
 import pymongo
 
 import sys
 sys.path.append('./LibraryWiki/')
 import app.entity_iterators
 import extract_ents
+from app.node_entities import Authority
 
 def get_some_records():
     """Example from Eyal's mail:
@@ -36,7 +38,7 @@ def get_some_records():
         **{'metadataprefix': 'marc21',
            'set': 'AUTREIMRC',
            'from': '2016-12-18T05:31:45Z',
-           'until': '2016-12-18T05:31:55Z',
+           'until': '2016-12-18T05:31:46Z',
           })
     return nli_records
 
@@ -93,18 +95,38 @@ def test_update_db_from_sickle_xml_output_file(c):
 
     print("docs in test_ents:", c.count())
 
+
+def xml_record_2_authority(record_str, xml_prefix=''):
+    '''convert an xml string received received from sickle
+
+    code copied and adapted from LibraryWiki's get_authorities function
+    '''
+    parsed_buf = xmltodict.parse(record_str, process_namespaces=False)
+    parsed_buf = parsed_buf['record']['metadata']
+    prefixed_record_str = '{xml_prefix}record'.format(xml_prefix=xml_prefix)
+    record = parsed_buf[prefixed_record_str]
+    result = {k: record[k] for k in record if
+            k == "{xml_prefix}controlfield".format(xml_prefix=xml_prefix) or
+            k == "{xml_prefix}datafield".format(xml_prefix=xml_prefix)}
+    if result.get('{xml_prefix}datafield'.format(xml_prefix=xml_prefix)):
+        return Authority(app.authorities.convert_dict(result, xml_prefix))
+    else:
+        return None
+
+
 if __name__ == "__main__":
 
     cl = pymongo.MongoClient('localhost', 27017)  # default port!
     db = cl['for_test']
     c = db.test_ents
 
-    test_update_db_from_sickle_xml_output_file(c)
+    #test_update_db_from_sickle_xml_output_file(c)
 
     nli_records = get_some_records()
 
     id_start = 'oai:aleph-nli:NNL10-'
-    for r in nli_records:
+    for i, r in enumerate(nli_records):
+        print('processing record: ', i)
         if r.deleted:
             raw_id = r.header.identifier
             assert raw_id.startswith(id_start)
@@ -112,4 +134,22 @@ if __name__ == "__main__":
             print('deleting: ', r_id)
             c.remove({'id': {"$eq": r_id}})
         else:  # update or create new record
-            pass
+            # print(i, r.header)
+            # print('r.raw:', r.raw)
+            # record_as_dict = xmltodict.parse(r.raw, process_namespaces=False)
+            # print('res from xmltodict:', record_as_dict)
+            # print("record_as_dict['record']['metadata']:", record_as_dict['record']['metadata'])
+            ent = xml_record_2_authority(r.raw, xml_prefix='marc:')
+            # print(ent)
+            try:
+                # print("in update_db - ent.data:", ent.data)
+                ent_data = extract_ents.extract_data_from_entity_dict(ent.data)
+                if ent_data:
+                    # pprint(ent_data)
+                    # Do we need to look at the result from update_one?
+                    c.update_one(filter={"id": ent_data["id"]},
+                            update={"$set": ent_data},
+                                 upsert=True)
+            except Exception as e:
+                print('exception from extract_data_from_entity_dict:', e)
+                pprint(ent.data)
